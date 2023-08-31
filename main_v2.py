746f61e4a6acedf0ef71a4e01ad26e4d26a93f19
Joaquin Chung <joaquinchung@gmail.com>
2023-08-31 14:00:46 -0500

    Part 2 of the tutorial complete

---
+++ main_v2.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from waggle.data.vision import Camera
+
+
+def compute_mean_color(image):
+    return np.mean(image, (0, 1)).astype(float)
+
+
+def main():
+    # open camera and take snapshot
+    with Camera() as camera:
+        snapshot = camera.snapshot()
+
+    # compute mean color
+    mean_color = compute_mean_color(snapshot.data)
+
+    # print mean color
+    print(mean_color)
+
+
+if __name__ == "__main__":
+    main()
